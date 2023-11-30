43f90f21341fc42371e2a2bf4ff4c5bf9b0e81a8
xFadedxShadow <63852447+xFadedxShadow@users.noreply.github.com>
2023-11-30 04:24:16 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ from configparser import ConfigParser
 config = ConfigParser()
 
 # xFadedxShadows Configuration File.
-config['xFadedxShadowsConfig'] = {
-    'base': ['base', 'base-devel', 'linux-zen', 'linux-zen-headers', 'linux-firmware', 'sudo', 'nano', 'xdg-user-dirs', 'amd-ucode'],
-    'audio_subsystem': ['pipewire', 'lib32-pipewire', 'pipewire-pulse', 'pipewire-audio', 'pipewire-alsa', 'pipewire-jack', 'lib32-pipewire-jack'],
-    'network': ['networkmanager'],
-    'bootloader': ['grub', 'efibootmgr'],
+config["xFadedxShadowsConfig"] = {
+    'base': ["base', 'base-devel', 'linux-zen', 'linux-zen-headers', 'linux-firmware', 'sudo', 'nano', 'xdg-user-dirs', 'amd-ucode"],
+    'audio_subsystem': ["pipewire', 'lib32-pipewire', 'pipewire-pulse', 'pipewire-audio', 'pipewire-alsa', 'pipewire-jack', 'lib32-pipewire-jack"],
+    'network': ["networkmanager"],
+    'bootloader': ["grub', 'efibootmgr"],
     'bootloader_cfg': 'configs/grub/nvidia',
-    'drivers': ['nvidia-dkms', 'libglvnd', 'nvidia-utils', 'opencl-nvidia', 'lib32-libglvnd', 'lib32-nvidia-utils', 'lib32-opencl-nvidia', 'nvidia-settings'],
+    'drivers': ["nvidia-dkms', 'libglvnd', 'nvidia-utils', 'opencl-nvidia', 'lib32-libglvnd', 'lib32-nvidia-utils', 'lib32-opencl-nvidia', 'nvidia-settings"],
     'drivers_cfg': 'configs/mkinitcpio/nvidia.conf',
     'pacman_hooks': 'config/hooks/nvidia.hook',
-    'post_packages': ['firefox', 'discord', 'steam', 'yuzu', 'p7zip', 'unrar', 'transmission-gtk', 'htop', 'neofetch'],
+    'post_packages': ["firefox', 'discord', 'steam', 'yuzu', 'p7zip', 'unrar', 'transmission-gtk', 'htop', 'neofetch"],
     'timezone': 'US/Eastern',
     'locale': 'en_US.UTF-8',
     'hostname': 'null-desktop',
-    'users': ['xfadedxshadow'],
-    'groups': ['wheel' ,'storage', 'power'],
-    'system_services': ['NetworkManager', 'bluetooth']
+    'users': ["xfadedxshadow"],
+    'groups': ["wheel' ,'storage', 'power"],
+    'system_services': ["NetworkManager', 'bluetooth"]
 }
 
 
@@ -50,69 +50,69 @@ def command(command):
 if __name__ == '__main__':
     write_config()
     read_config('config.cfg')
-    config_data = config['xFadedxShadowsConfig']
+    config_data = config["xFadedxShadowsConfig"]
     
     # Runs pacstrap for base packages.
-    for package in config['base']:
+    for package in config["base"]:
         command(f'sudo pacstrap -K {package}')
     
     # Generate fstab
     command(f'sudo genfstab -U /{root_parition} >> /{root_parition}/etc/fstab')
     
     # Installs audio subsystem
-    for package in config['audio_subsystem']:
+    for package in config["audio_subsystem"]:
         command(f'sudo arch-chroot /{root_parition} sudo pacman -S {package}')
     
     # Installs networking
-    for package in config['network']:
+    for package in config["network"]:
         command(f'sudo arch-chroot /{root_parition} sudo pacman -S {package}')
     
     # Installs bootloader
-    for package in config['bootloader']:
+    for package in config["bootloader"]:
         command(f'sudo arch-chroot /{root_parition} sudo pacman -S {package}')
     
     command(f'sudo arch-chroot /{root_parition} sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB')
 
     
     # Configure bootloader [Check if config is empty and decide to run]
-    #command(f'sudo arch-chroot /{root_parition} sudo cp {config['bootloader_cfg']} >> /etc/default/grub')
+    #command(f'sudo arch-chroot /{root_parition} sudo cp {config["bootloader_cfg"]} >> /etc/default/grub')
     
     # Installs drivers
-    #for package in config['drivers']:
+    #for package in config["drivers"]:
     #    command(f'sudo arch-chroot /{root_parition} sudo pacman -S {package}')
     
     # Configure drivers [Check if config is empty and decide to run]
-    #command(f'sudo arch-chroot /{root_parition} sudo cp {config['drivers_cfg']} >> /etc/mkinitcpio.conf')
+    #command(f'sudo arch-chroot /{root_parition} sudo cp {config["drivers_cfg"]} >> /etc/mkinitcpio.conf')
     
     # Installs additional packages
-    for package in config['post_packages']:
+    for package in config["post_packages"]:
         command(f'sudo arch-chroot /{root_parition} sudo pacman -S {package}')
     
     # Configures pacman hooks. [Check if hooks is empty or not and is nvidia]
     #command(f'sudo arch-chroot /{root_parition} sudo mkdir /etc/pacman.d/hooks')
-    #command(f'sudo arch-chroot /{root_parition} sudo cp {config['pacman_hooks']} >> /etc/pacman.d/hooks/nvidia.hook')
+    #command(f'sudo arch-chroot /{root_parition} sudo cp {config["pacman_hooks"]} >> /etc/pacman.d/hooks/nvidia.hook')
 
     # Configure timezone
-    command(f'sudo arch-chroot /{root_parition} sudo timedatectl set-timezone {config['timezone']}')
-    command(f'sudo arch-chroot /{root_parition} sudo ln -sf /usr/share/zoneinfo/{config['timezone']} /etc/localtime')
+    command(f'sudo arch-chroot /{root_parition} sudo timedatectl set-timezone {config["timezone"]}')
+    command(f'sudo arch-chroot /{root_parition} sudo ln -sf /usr/share/zoneinfo/{config["timezone"]} /etc/localtime')
     command(f'sudo arch-chroot /{root_parition} sudo hwclock --systohc')
 
     # Configure locales
-    command(f'sudo arch-chroot /{root_parition} sudo echo "{config['locale']}" >> /etc/locale.gen')
+    command(f'sudo arch-chroot /{root_parition} sudo echo "{config["locale"]}" >> /etc/locale.gen')
     command(f'sudo arch-chroot /{root_parition} sudo echo "LANG={config[locale]}" >> /etc/locale.conf')
     command(f'sudo arch-chroot /{root_parition} sudo locale-gen')
 
     # Configure hostname
-    command(f'sudo arch-chroot /{root_parition} sudo echo "{config['hostname']}" >> /etc/hostname')
+    command(f'sudo arch-chroot /{root_parition} sudo echo "{config["hostname"]}" >> /etc/hostname')
 
     # Configure users
-    for user in config['users']:
+    for user in config["users"]:
         command(f'sudo arch-chroot /{root_parition} sudo usermod -m {user}')
-        for group in config['groups']:
+        for group in config["groups"]:
             command(f'sudo arch-chroot /{root_parition} sudo usermod -aG {group} {user}')
     
     # Enable system services
-    for service in config['system_services']:
+    for service in config["system_services"]:
         command(f'sudo arch-chroot /{root_parition} sudo systemctl enable {service}')
     
     # Regenerate initramfs & grub configuration
