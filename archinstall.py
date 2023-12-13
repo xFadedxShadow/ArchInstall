62e215d6e0d6d30b74e48a1347568cc0278f3bd8
xFadedxShadow <63852447+xFadedxShadow@users.noreply.github.com>
2023-12-13 11:08:03 -0500

    Add files via upload

---
+++ archinstall.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import json
+import argparse
+import subprocess
+
+from core.base import PackageManager, ConfigManager, SystemConfig
+
+
+# Define arguments
+def define_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='Select a customized configuration.')
+    args = parser.parse_args()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    if (args.config) == None:
+        config = ConfigManager.load_config('configs/default.json')
+        print('Loaded "configs/default.json" configuration.')
+    else:
+        config = ConfigManager.load_config(args.config)
+        print(f'Loaded "{args.config}" configuration.')
+
+
+    PackageManager.pacstrap_install('/', config['base']) # Install base arch system
+
+    PackageManager.chroot_install('/', config['additional_packages']) # Install additional packages on top of base system
+
+    # PackageManager.chroot_uninstall() | Uninstall packages from base system
+
+    SystemConfig.config_timezone('/', config['timezone']) # Configure timezone
+
+    SystemConfig.config_locales('/', config['locale']) # Configure locales
+
+    SystemConfig.config_hostname('/', config['hostname']) # Configure hostname
+
+    SystemConfig.config_users('/', config['users']) # Configures Users
+
+    SystemConfig.config_grub('/', '/boot/efi') # Installs GRUB
+
+    SystemConfig.enable_services('/', config['services']) # Configure services while booted into OS
+
+    # PackageManager.install() | Install packages while booted into OS
+
+    # PackageManager.uninstall() | Uninstall packages while booted into OS
